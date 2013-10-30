997606ad1357c04db4ffb1d9fc5254648af2e1fd
zhangsanji <zhangsanji@meituan.com>
2013-10-30 19:17:09 +0800

    编辑试题功能ok；结构和样式调整

--- routes/subject.js
+++ routes/subject.js
@@ -5,19 +5,19 @@ var dbClient = require('mongodb').MongoClient,
 var CONST = {
     DB_CONNECT: 'mongodb://sanji.com:27017/mud-quiz',
     DB_COLLECTION: 'subjects',
-    DB_COLUMNS: ["q-question", "q-code", "q-tag", "a-answer", "a-code", "create-time", "modify-time"]
+    DB_COLUMNS: ['q-question', 'q-code', 'q-tag', 'a-answer', 'a-code', 'create-time', 'modify-time']
 };
 
 exports.new = function (req, res) {
     if (req.route.method === 'get') {
-        res.render("subject-new");
+        res.render('subject-new');
         return;
     }
     if (req.route.method === 'post') {
         var t = +Date.now(),
             data = {};
 
-        for(var len = CONST.DB_COLUMNS.length - 1;len >= 0; len--) {
+        for(var len = CONST.DB_COLUMNS.length - 1; len >= 0; len--) {
             var key = CONST.DB_COLUMNS[len];
             if (req.body.hasOwnProperty(key)) {
                 data[key] = req.body[key];
@@ -34,15 +34,15 @@ exports.new = function (req, res) {
                     throw err;
                 }
                 db.close();
-                res.redirect("/subject/" + docs[0]._id);
-                return;
+                res.redirect('/subject/' + docs[0]._id);
             });
         });
     }
 };
 
 exports.list = function (req, res) {
-    var id = req.params.id;
+    var id = req.params.id,
+        isModify = !!(typeof req.query.modify !== 'undefined');
     dbClient.connect(CONST.DB_CONNECT, function (err, db) {
         if (err) {
             throw err;
@@ -54,10 +54,33 @@ exports.list = function (req, res) {
             }
             db.close();
             if (docs === null) {
-                res.redirect("/subject");
+                res.redirect('/subject');
                 return;
             }
-            res.render("subject-list", {data: [docs]});
+            if (isModify) {
+                var tags = {};
+                for(var obj = docs['q-tag'], len = obj.length - 1; len >= 0; len--) {
+                    if ('javascript' === obj[len]) {
+                        tags.isJs = true;
+                    }
+                    else if ('html' === obj[len]) {
+                        tags.isHtml = true;
+                    }
+                    else if ('css' === obj[len]) {
+                        tags.isCss = true;
+                    }
+                    else if ('java' === obj[len]) {
+                        tags.isJava = true;
+                    }
+                    else {
+                        tags.other = obj[len];
+                    }
+                }
+                res.render('subject-modify', {data: docs, tags: tags});
+            }
+            else {
+                res.render('subject-list', {data: [docs]});
+            }
         });
     });
 };
@@ -68,27 +91,50 @@ exports.listAll = function(req, res){
             throw err;
         }
         var collection = db.collection(CONST.DB_COLLECTION);
-        collection.find().toArray(function(err, docs) {
+        collection.find().sort({'modify-time': -1}).toArray(function(err, docs) {
             if (err) {
                 throw err;
             }
             db.close();
             if (docs.length === 0) {
-                res.redirect("/subject");
+                res.redirect('/subject');
                 return;
             }
-            res.render("subject-list", {data: docs});
+            res.render('subject-list', {data: docs});
         });
     });
 };
 
 exports.modify = function (req, res) {
     var id = req.params.id;
-    res.render("subject-list", {text: "modify subject " + id});
+    var t = +Date.now(),
+        data = {};
+
+    for(var len = CONST.DB_COLUMNS.length - 1; len >= 0; len--) {
+        var key = CONST.DB_COLUMNS[len];
+        if (req.body.hasOwnProperty(key)) {
+            data[key] = req.body[key];
+        }
+    }
+    data['modify-time'] = t;
+    dbClient.connect(CONST.DB_CONNECT, function (err, db) {
+        if (err) {
+            throw err;
+        }
+        var collection = db.collection(CONST.DB_COLLECTION);
+        collection.update({_id: ObjectID(id)}, {$set: data}, {safe: true}, function (err, count) {
+            if (err) {
+                throw err;
+            }
+            db.close();
+            res.redirect('/subject/' + id);
+        });
+    });
+
 };
 
 
 exports.remove = function (req, res) {
     var id = req.params.id;
-    res.render("subject-list", {text: "remove subject " + id});
+    res.render('subject-list', {text: 'remove subject ' + id});
 };
